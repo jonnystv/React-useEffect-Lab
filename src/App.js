410783b2fd0748430a07bec1cdef1b2b4c903f06
jonnystv <jonathan@darefoundation.com>
2021-03-02 14:46:44 +0000

    Lab end

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
 
   const [counter, setCounter] = useState(1);
+  let [modifiedCounter, setModifiedCounter] = useState(1);
+
+
+  useEffect(() => {
+    console.log("useEffect Triggered");
+    setModifiedCounter(executeFizzBuzz)
+  }, [modifiedCounter])
+
+  const executeFizzBuzz = () => {
+
+    if (counter % 3 == 0 && counter % 5 == 0) {
+      setModifiedCounter = "FizzBuzz";
+    }
+    else if (counter % 5 == 0) {
+      setModifiedCounter = "Buzz";
+    }
+    else if (counter % 3 == 0) {
+      setModifiedCounter = "Fizz";
+    }
+    else {
+      setModifiedCounter = counter;
+    }
+  }
+  // useEffect(() => {
+  //   setDoubleNumber(number * 2)
+  // },[number]
+
+
+  // if (time < 10) {
+  //   greeting = "Good morning";
+  // } else if (time < 20) {
+  //   greeting = "Good day";
+  // } else {
+  //   greeting = "Good evening";
+  // }  
+
 
   const handleIncrement = () => {
     setCounter(counter + 1);
   }
 
-   return (
+  return (
 
     <>
-    
+
       <button value={counter} onClick={handleIncrement}>Increase number by one</button>
-        <h2>Output: {counter}</h2>
+      <h2>Output: {counter}</h2>
+      <h2>Modified Counter: {modifiedCounter}</h2>
     </>
 
   );
@@ -22,3 +59,10 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+
