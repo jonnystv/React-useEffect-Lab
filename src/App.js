18c996654714465595ce453b9c953e726b888eb9
jonnystv <jonathan@darefoundation.com>
2021-03-02 14:07:18 +0000

    initial commit - number incrementing by one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const [counter, setCounter] = useState(1);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  }
+
+   return (
+
+    <>
+    
+      <button value={counter} onClick={handleIncrement}>Increase number by one</button>
+        <h2>Output: {counter}</h2>
+    </>
+
   );
+
 }
 
 export default App;
